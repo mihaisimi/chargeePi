93f21f9173c17e6f2ce24f763625d4e26a0c4f62
Adrian Cuzman <adriancuzman@gmail.com>
2022-06-11 20:32:28 +0300

    added rpi detection and charging timeout and regex licence checking

--- src/image_feed/alpr_test.py
+++ src/image_feed/alpr_test.py
@@ -1,10 +1,46 @@
 import cv2
 import json
 from openalpr import Alpr
+from datetime import datetime
+import re
+import io
+import os
 
 cap= cv2.VideoCapture(0)
 
+last_seen = 0
 last_plate_detected = ''
+charging = False
+
+def is_raspberrypi():
+    if os.name != 'posix':
+        return False
+    chips = ('BCM2708','BCM2709','BCM2711','BCM2835','BCM2836')
+    try:
+        with io.open('/proc/cpuinfo', 'r') as cpuinfo:
+            for line in cpuinfo:
+                if line.startswith('Hardware'):
+                    _, value = line.strip().split(':', 1)
+                    value = value.strip()
+                    if value in chips:
+                        return True
+    except Exception:
+        pass
+    return False
+
+def enable_charging():
+    charging = True
+    print("CHARGIN ENABLED")
+    if (is_raspberrypi()):
+        print("change gpio")
+
+def disabled_charging():
+    charging = False
+    print("DISABLE_CHARGING")
+    if (is_raspberrypi()):
+        print("change gpio")
+
+
 
 def checkJudet(str):
     judete=["B", "AB", "AG", "AR", "BC", "BH", "BN", "BR", "BT", "BV", "BZ", "CJ", "CL", "CS", "CT", "CV", "DB", "DJ", "GJ", "GL", "GR", "HD", "HR", "IF", "IL", "IS", "MH", "MM", "MS", "NT", "OT", "PH", "SB", "SJ", "SM", "SV", "TL", "TM", "TR", "VL", "VN", "VS"]
@@ -24,20 +60,31 @@ while True:
     ret,enc = cv2.imencode("*.bmp", frame)
     results = alpr.recognize_array(bytes(bytearray(enc)))
     #print(json.dumps(results, indent=4))
+    now = datetime.now().timestamp() * 1000
     if (len(results['results'])>0):
         bestResult = results['results'][0]
         if (bestResult['confidence'] > 88):
-            if (len(bestResult['plate']) > 6 and len(bestResult['plate']) < 9 and checkJudet(bestResult['plate'].upper())):
-                if (last_plate_detected != bestResult['plate']):
+            plateMatch = re.search("[A-Z][A-Z][0-9][0-9][A-Z][A-Z][A-Z]",bestResult['plate'])
+
+            if (plateMatch and len(bestResult['plate']) > 6 and len(bestResult['plate']) < 9 and checkJudet(bestResult['plate'].upper())):
+                if (last_plate_detected != bestResult['plate'] and not charging):
                     print("-------------------------------")
                     print("detected plate:"+bestResult['plate'])
                     print("confidence:"+str(bestResult['confidence']))
                     last_plate_detected = bestResult['plate']
                     print("-------------------------------")
+                    last_seen = now
+                    enable_charging()
+                else:
+                    last_seen = now
+
     else:
         if (len(last_plate_detected) > 0):
-            print("licence lost: "+last_plate_detected)
-            last_plate_detected=""
+            last_seen_dif = now - last_seen
+            if ( (last_seen_dif) > 10000):
+                print("licence lost: "+last_plate_detected)
+                last_plate_detected=""
+                disabled_charging()
         
     if cv2.waitKey(1) == 13:
             break
