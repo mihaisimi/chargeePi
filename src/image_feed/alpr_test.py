333ee6a30e49b42b91a6c9a7ec7787d55ec5813b
Adrian Cuzman <adriancuzman@gmail.com>
2022-06-11 19:54:41 +0300

    added judet checking

--- src/image_feed/alpr_test.py
+++ src/image_feed/alpr_test.py
@@ -4,6 +4,16 @@ from openalpr import Alpr
 
 cap= cv2.VideoCapture(0)
 
+last_plate_detected = ''
+
+def checkJudet(str):
+    judete=["B", "AB", "AG", "AR", "BC", "BH", "BN", "BR", "BT", "BV", "BZ", "CJ", "CL", "CS", "CT", "CV", "DB", "DJ", "GJ", "GL", "GR", "HD", "HR", "IF", "IL", "IS", "MH", "MM", "MS", "NT", "OT", "PH", "SB", "SJ", "SM", "SV", "TL", "TM", "TR", "VL", "VN", "VS"]
+    for j in judete:
+        if (str.startswith(j)):
+            return True
+    return False
+
+
 alpr = Alpr("eu", "/etc/openalpr/openalpr.conf", "/usr/share/openalpr/runtime_data")
 if not alpr.is_loaded():
     print("Error loading OpenALPR")
@@ -17,11 +27,17 @@ while True:
     if (len(results['results'])>0):
         bestResult = results['results'][0]
         if (bestResult['confidence'] > 88):
-            if (len(bestResult['plate']) > 6 and len(bestResult['plate']) < 9):
-                print("-------------------------------")
-                print("plate:"+bestResult['plate'])
-                print("confidence:"+str(bestResult['confidence']))
-                print("-------------------------------")
+            if (len(bestResult['plate']) > 6 and len(bestResult['plate']) < 9 and checkJudet(bestResult['plate'].upper())):
+                if (last_plate_detected != bestResult['plate']):
+                    print("-------------------------------")
+                    print("detected plate:"+bestResult['plate'])
+                    print("confidence:"+str(bestResult['confidence']))
+                    last_plate_detected = bestResult['plate']
+                    print("-------------------------------")
+    else:
+        if (len(last_plate_detected) > 0):
+            print("licence lost: "+last_plate_detected)
+            last_plate_detected=""
         
     if cv2.waitKey(1) == 13:
             break
